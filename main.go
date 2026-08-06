3209215dfe39662fca4927d9459d5db5b6c19be2
ErenKarakus1 <eren1karakus@gmail.com>
2026-08-06 20:25:22 +0300

    feat: add expense creation and listing endpoints

--- main.go
+++ main.go
@@ -1,18 +1,65 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func health(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "ok"})
 }
 
-func main() {
-	router := gin.Default()
-	router.GET("/health", health)
-	router.Run(":8080")
+func validateExpense(req CreateExpenseRequest) error {
+	if req.AmountCents <= 0 {
+		return errors.New("Couldnt create expense, amount must be bigger than zero")
+	}
+	return nil
+}
 
+func buildExpense(c *gin.Context) (Expense, error) {
+	var req CreateExpenseRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		return Expense{}, errors.New("Couldnt create expense, invalid request body")
+	}
+	if err := validateExpense(req); err != nil {
+		return Expense{}, err
+	}
+	expense := Expense{
+		ID:          uuid.New(),
+		CreatedAt:   time.Now(),
+		AmountCents: req.AmountCents,
+		Category:    req.Category,
+		Description: req.Description,
+	}
+
+	return expense, nil
+
+}
+
+func main() {
+	expenses := []Expense{}
+
+	router := gin.Default()
+
+	router.GET("/health", health)
+
+	router.POST("/expenses", func(c *gin.Context) {
+		expense, err := buildExpense(c)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		expenses = append(expenses, expense)
+		c.JSON(http.StatusCreated, expense)
+	})
+
+	router.GET("/expenses", func(c *gin.Context) {
+		c.JSON(http.StatusOK, expenses)
+	})
+
+	router.Run(":8080")
 }
--- models.go
+++ models.go
@@ -7,9 +7,15 @@ import (
 )
 
 type Expense struct {
-	ID          uuid.UUID
-	UserID      uuid.UUID
-	AmountCents int64
-	Description string
-	CreatedAt   time.Time
+	ID          uuid.UUID `json:"id"`
+	AmountCents int64     `json:"amountcents"`
+	Category    string    `json:"category"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"createdat"`
+}
+
+type CreateExpenseRequest struct {
+	AmountCents int64  `json:"amountcents"`
+	Category    string `json:"category"`
+	Description string `json:"description"`
 }
